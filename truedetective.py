f02519ea7e92bba0a85affa92cf9e3de35711a8a
Gabi <gabriel_marian22@yahoo.com>
2020-03-23 14:00:42 +0200

    made is_sunday function

--- truedetective.py
+++ truedetective.py
@@ -7,39 +7,46 @@ def is_leap_year(year):
 
 
 def is_sunday(day, weekday_of_first):
-    if weekday_of_first == 'M':
-        weekday_of_first = 1
-        if ((weekday_of_first - 1) + day) % 7 == 0 and (weekday_of_first - 1) + day <= 31:
-            return True
-    if weekday_of_first == 'T':
-        weekday_of_first = 2
-        if ((weekday_of_first - 1) + day) % 7 == 0 and (weekday_of_first - 1) + day <= 31:
-            return True
-    if weekday_of_first == 'W':
-        weekday_of_first = 3
-        if ((weekday_of_first - 1) + day) % 7 == 0 and (weekday_of_first - 1) + day <= 31:
-            return True
-    if weekday_of_first == 'Th':
-        weekday_of_first = 4
-        if ((weekday_of_first - 1) + day) % 7 == 0 and (weekday_of_first - 1) + day <= 31:
-            return True
-    if weekday_of_first == 'F':
-        weekday_of_first = 5
-        if ((weekday_of_first - 1) + day) % 7 == 0 and (weekday_of_first - 1) + day <= 31:
-            return True
-    if weekday_of_first == 'Sa':
-        weekday_of_first = 6
-        if ((weekday_of_first - 1) + day) % 7 == 0 and (weekday_of_first - 1) + day <= 31:
-            return True
-    if weekday_of_first == 'S':
-        weekday_of_first = 7
-        if ((weekday_of_first - 1) + day) % 7 == 0 and (weekday_of_first - 1) + day <= 31:
+    days = {'M': 1, 'T': 2, 'W': 3, 'Th': 4, 'F': 5, 'Sa': 6, 'S': 7}
+    for key in days.keys():
+        if (weekday_of_first == key) and (((days[key] - 1) + day) % 7 == 0) and ((days[key] - 1) + day <= 31):
             return True
     else:
         return False
 
+    # if weekday_of_first == 'M':
+    #     weekday_of_first = 1
+    #     if ((weekday_of_first - 1) + day) % 7 == 0 and (weekday_of_first - 1) + day <= 31:
+    #         return True
+    # if weekday_of_first == 'T':
+    #     weekday_of_first = 2
+    #     if ((weekday_of_first - 1) + day) % 7 == 0 and (weekday_of_first - 1) + day <= 31:
+    #         return True
+    # if weekday_of_first == 'W':
+    #     weekday_of_first = 3
+    #     if ((weekday_of_first - 1) + day) % 7 == 0 and (weekday_of_first - 1) + day <= 31:
+    #         return True
+    # if weekday_of_first == 'Th':
+    #     weekday_of_first = 4
+    #     if ((weekday_of_first - 1) + day) % 7 == 0 and (weekday_of_first - 1) + day <= 31:
+    #         return True
+    # if weekday_of_first == 'F':
+    #     weekday_of_first = 5
+    #     if ((weekday_of_first - 1) + day) % 7 == 0 and (weekday_of_first - 1) + day <= 31:
+    #         return True
+    # if weekday_of_first == 'Sa':
+    #     weekday_of_first = 6
+    #     if ((weekday_of_first - 1) + day) % 7 == 0 and (weekday_of_first - 1) + day <= 31:
+    #         return True
+    # if weekday_of_first == 'S':
+    #     weekday_of_first = 7
+    #     if ((weekday_of_first - 1) + day) % 7 == 0 and (weekday_of_first - 1) + day <= 31:
+    #         return True
+    # else:
+    #     return False
 
-print(is_sunday(7, 'W'))
+
+print(is_sunday(7, 'M'))
 
 
 def should_bring_umbrella(rains, wind_scale, cloudy, red_sky, strong_flower_smell, spiders_down, lying_cattle, strong_sunshine):
